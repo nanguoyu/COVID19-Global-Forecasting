4a5aaf9888876bd43178626c35f3c55731dfbc9b
Dong <5096749+nanguoyu@users.noreply.github.com>
2020-04-21 21:29:57 +0200

    Modify DataLoader test

--- tests/testDataLoader.py
+++ tests/testDataLoader.py
@@ -7,7 +7,10 @@ from WorkFlow.core.DataLoader import DataLoader
 
 file = 'D:/Dropbox/Project/code/Python/COVID19-Global-Forecasting/data/train_flight.csv'
 split = 0.8
+print("[Test]:Load data from ", file)
 data_loader = DataLoader(filename=file, split=split)
 trainX, trainY = data_loader.get_train_data(seq_len=7, normalise=False)
-
-print("trainX shape", trainX.shape, "trainY shape", trainY.shape)
+valX, valY = data_loader.get_evaluate_data(seq_len=7, normalise=False)
+print("[Test]: trainX shape", trainX.shape, "trainY shape", trainY.shape)
+print("[Test]: valX shape", valX.shape, "valY shape", valY.shape)
+print("[Test]:Done")
